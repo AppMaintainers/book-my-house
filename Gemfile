189d49549ead4c10ee78ca4a313447f36ce1183c
Gregurevic <paul.greg.indy@gmail.com>
2025-02-25 13:27:05 +0100

    bump rails to 8.0

--- Gemfile
+++ Gemfile
@@ -3,7 +3,7 @@
 source 'https://rubygems.org'
 
 ruby '3.3.4'
-gem 'rails', '~> 7.2.2.1'
+gem 'rails', '~> 8.0.1'
 
 gem 'bootsnap', require: false
 gem 'factory_bot_rails'
--- db/schema.rb
+++ db/schema.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -12,21 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 20_210_212_111_039) do
-  create_table 'bookings', force: :cascade do |t|
-    t.date 'day'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.integer 'house_id'
-    t.index ['house_id'], name: 'index_bookings_on_house_id'
+ActiveRecord::Schema[8.0].define(version: 2021_02_12_111039) do
+  create_table "bookings", force: :cascade do |t|
+    t.date "day"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "house_id"
+    t.index ["house_id"], name: "index_bookings_on_house_id"
   end
 
-  create_table 'houses', force: :cascade do |t|
-    t.string 'name'
-    t.string 'city'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
+  create_table "houses", force: :cascade do |t|
+    t.string "name"
+    t.string "city"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  add_foreign_key 'bookings', 'houses'
+  add_foreign_key "bookings", "houses"
 end
