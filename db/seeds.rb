a67bd970af0e8e8cff262c63b61e3726777faed5
Gregurevic <paul.greg.indy@gmail.com>
2025-02-25 12:44:55 +0100

    use database_cleaner in seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,12 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 
+require 'database_cleaner/active_record'
+
+# Clear DB
+DatabaseCleaner[:active_record].strategy = :truncation
+DatabaseCleaner[:active_record].clean
+
 House.create(name: 'Summer house', city: 'Badacsonytomaj')
 House.create(name: 'Flat', city: 'Budapest')
 House.create(name: 'Family house', city: 'Győr')
