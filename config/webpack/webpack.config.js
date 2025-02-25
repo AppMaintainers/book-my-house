515a70deb3f39f34bfc29839f499d50095368c27
Gregurevic <paul.greg.indy@gmail.com>
2025-02-25 14:10:32 +0100

    add test workflow

--- config/webpack/webpack.config.js
+++ config/webpack/webpack.config.js
@@ -1,2 +1,6 @@
-const { generateWebpackConfig } = require('shakapacker');
-module.exports = generateWebpackConfig();
+// See the shakacode/shakapacker README and docs directory for advice on customizing your webpackConfig.
+const { generateWebpackConfig } = require('shakapacker')
+
+const webpackConfig = generateWebpackConfig()
+
+module.exports = webpackConfig
